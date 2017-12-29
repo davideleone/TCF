91d4a51472557d66b9b82760dee4cbe9742ae243
Albi <alferiot@gmail.com>
2017-12-29 12:53:59 +0100

    docker local fix

--- dockerfile
+++ dockerfile
@@ -1,4 +1,3 @@
 FROM nginx
 COPY ./dist/* /usr/share/nginx/html/
-#RUN cd /usr/share/nginx/html/ && npm install
 EXPOSE 8080
--- src/environments/environment.docker.ts
+++
@@ -1,9 +0,0 @@
-import { JL } from 'jsnlog';
-
-export const environment = {
-  production: true,
-  apiUrl: 'https://ftc.finconsgroup.com',
-  loggerApiUrl: '/tcf/api/jsnlog.logger',
-  logLevel: JL.getDebugLevel()
-};
-
